9f45f431a237ad259da4996a0cfc98a354989aa7
AELCharis <AELCharis>
2019-10-12 10:58:13 +0300

    Added dynamic text from Object

--- website/src/app/configuration.ts
+++ website/src/app/configuration.ts
@@ -7,5 +7,34 @@ export const configuration = {
     buttontext: 'do somthing',
     buttonlink: '\home'
   },
+  intro: {
+    tagline: 'SUCESS',
+    title: 'How We Help You To Sell Your Product',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do\n' +
+      '          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam!',
+  },
+  services: {
+    tagline: 'BELIEVING',
+    title: 'Focusing On What Matters Most\n',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam!\n' +
+      '\n',
+  },
+  testimonials: {
+    tagline: 'FEEDBACK',
+    title: 'How We Help You To Sell Your Product',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do\n' +
+      '          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam!',
+  },
+  clients: {
+    tagline: 'TRUST',
+    title: 'Companies who use our services\n',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam!\n' +
+      '\n',
+  },
+  pricing: {
+    tagline: 'YOUR CHOICE\n',
+    title: 'We have the right package for you\n',
+    description: '',
+  },
 
 };
